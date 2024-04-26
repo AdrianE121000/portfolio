6e108f3f7f3ac710feba56e963e78bd8166af20c
AdrianE121000 <paracosa3s@gmail.com>
2024-04-26 09:33:13 -0400

    update skills

--- src/data/constants.jsx
+++ src/data/constants.jsx
@@ -3,6 +3,7 @@ import { ImGit, ImHtmlFive } from 'react-icons/im';
 import {
   SiCss3,
   SiJavascript,
+  SiTypescript,
   SiMarkdown,
   SiNotion,
   SiReact,
@@ -34,7 +35,27 @@ export const skills = [
       },
       {
         name: 'Nextjs',
-        image: <TbBrandNextjs />,
+        image: <TbBrandNextjs size={24} />,
+      },
+      {
+        name: 'Astro',
+        image: (
+          <svg
+            viewBox='0 0 256 366'
+            xmlns='http://www.w3.org/2000/svg'
+            width='24'
+            height='24'
+            preserveAspectRatio='xMidYMid'>
+            <path
+              fill='#fff'
+              d='M182.022 9.147c2.982 3.702 4.502 8.697 7.543 18.687L256 246.074a276.467 276.467 0 0 0-79.426-26.891L133.318 73.008a5.63 5.63 0 0 0-10.802.017L79.784 219.11A276.453 276.453 0 0 0 0 246.04L66.76 27.783c3.051-9.972 4.577-14.959 7.559-18.654a24.541 24.541 0 0 1 9.946-7.358C88.67 0 93.885 0 104.314 0h47.683c10.443 0 15.664 0 20.074 1.774a24.545 24.545 0 0 1 9.95 7.373Z'
+            />
+            <path
+              fill='#FF5D01'
+              d='M189.972 256.46c-10.952 9.364-32.812 15.751-57.992 15.751-30.904 0-56.807-9.621-63.68-22.56-2.458 7.415-3.009 15.903-3.009 21.324 0 0-1.619 26.623 16.898 45.14 0-9.615 7.795-17.41 17.41-17.41 16.48 0 16.46 14.378 16.446 26.043l-.001 1.041c0 17.705 10.82 32.883 26.21 39.28a35.685 35.685 0 0 1-3.588-15.647c0-16.886 9.913-23.173 21.435-30.48 9.167-5.814 19.353-12.274 26.372-25.232a47.588 47.588 0 0 0 5.742-22.735c0-5.06-.786-9.938-2.243-14.516Z'
+            />
+          </svg>
+        ),
       },
       {
         name: 'HTML',
@@ -47,6 +68,10 @@ export const skills = [
       {
         name: 'JavaScript',
         image: <SiJavascript color='#ead41c' />,
+      },
+      {
+        name: 'TypeScript',
+        image: <SiTypescript color='#007acc' />,
       },
       {
         name: 'Tailwindcss',
@@ -98,7 +123,7 @@ export const projects = [
     },
     image:
       'https://github.com/AdrianE121000/portfolio/blob/master/public/learndDeals.jpg?raw=true',
-    tags: ['React', 'API', 'Tailwind', 'Python'],
+    tags: ['React', 'Tailwind', 'Python'],
     member: [
       {
         name: 'Adrian Pérez',
@@ -121,7 +146,7 @@ export const projects = [
       en: 'Web application that allows users to take a quiz to measure their skills in different programming languages ​​(for now only JavaScript and Python, others pending to be added)',
     },
     image: 'https://adriane121000.github.io/portfolio/quiz-app.jpg',
-    tags: ['React', 'Zustand', 'MaterialUI', 'State'],
+    tags: ['React', 'Zustand', 'MaterialUI'],
     member: [
       {
         name: 'Adrian Pérez',
@@ -139,7 +164,7 @@ export const projects = [
       en: 'Web application that allows you to show, add, delete and edit users, as well as obtain random users from an API, saves all users in the localStorage, also has pagination and light and dark mode to improve the user experience.',
     },
     image: 'https://adriane121000.github.io/portfolio/user-manager.jpg',
-    tags: ['React', 'Zustand', 'Tailwind', 'Fetch'],
+    tags: ['React', 'Zustand', 'Tailwind'],
     member: [
       {
         name: 'Adrian Pérez',
@@ -157,7 +182,7 @@ export const projects = [
       en: 'Web application that allows users to add different tasks, as well as select those that have been completed and filter them according to their status (completed or not completed) ',
     },
     image: 'https://adriane121000.github.io/portfolio/todo_app.jpg',
-    tags: ['React', 'Tailwindcss', 'Context', 'State'],
+    tags: ['React', 'Tailwindcss'],
     member: [
       {
         name: 'Adrian Pérez',
@@ -175,7 +200,7 @@ export const projects = [
       en: 'Web application that shows a list of cryptocurrency prices. The CoinGecko API is used to obtain up-to-date data on cryptocurrency prices and display them in an easy-to-understand format for users.',
     },
     image: 'https://adriane121000.github.io/portfolio/crypto_market.jpg',
-    tags: ['React', 'API', 'Material UI', 'State'],
+    tags: ['React', 'Material UI'],
     member: [
       {
         name: 'Adrian Pérez',
@@ -194,7 +219,7 @@ export const projects = [
       en: 'Simple web application that allows you to enjoy the game tic tac toe from your browser. Additionally, you can save the game in the localStorage to continue later. (Pending modality 1 vs AI)',
     },
     image: 'https://adriane121000.github.io/portfolio/tic-tac-toe.jpg',
-    tags: ['React', 'localStorage', 'State'],
+    tags: ['React'],
     member: [
       {
         name: 'Adrian Pérez',
@@ -212,7 +237,7 @@ export const projects = [
       en: 'Web application that allows users to search for any country and instantly access detailed information such as population, capital, currency and more. Simply enter the country name in the search bar, and our app will pull the data from an external API to provide you with all the relevant details. Explore the world at your fingertips with CountryInfo!',
     },
     image: 'https://adriane121000.github.io/portfolio/countries-world.jpg',
-    tags: ['React', 'API', 'State', 'useRef'],
+    tags: ['React'],
     member: [
       {
         name: 'Adrian Pérez',
